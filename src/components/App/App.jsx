8da86e3c63bdedad55d2af2930950dbc988847ce
Jazzmine-Flora <taliba.latif@gmail.com>
2024-09-24 16:40:11 -0400

    rendering cards accord temp

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,7 @@ import Header from "../Header/Header";
 import Main from "../Main/Main";
 
 function App() {
-  const { weatherData, setWeatherData } = useState({ type: "hot" });
+  const [weatherData, setWeatherData] = useState({ type: "cold" });
 
   return (
     <div className="page">
