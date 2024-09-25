9e8f6185e2650230de0d7eec0f111d77464e0475
Jazzmine-Flora <taliba.latif@gmail.com>
2024-09-25 16:36:39 -0400

    moved to app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,7 +14,64 @@ function App() {
         <Header />
         <Main weatherData={weatherData} />
       </div>
-      <ModalWithForm />
+      <ModalWithForm buttonText="Add garment" title="New garment">
+        <label htmlFor="name" className="modal__label">
+          <p className="input__title">Name</p>
+          <input
+            className="modal__input"
+            type="text"
+            id="name"
+            placeholder="Name"
+            required
+          />
+        </label>
+        <label htmlFor="imageUrl" className="modal__label">
+          <p className="input__title">Image</p>
+          <input
+            className="modal__input"
+            type="url"
+            id="imageUrl"
+            placeholder="Image URL"
+            required
+          />
+        </label>
+        <fieldset className="modal__fieldset">
+          <legend className="modal__legend">Select the weather type:</legend>
+          <label htmlFor="hot" className="modal__label modal__label_type_radio">
+            <input
+              className="modal__radio-input"
+              type="radio"
+              id="hot"
+              required
+            />
+            Hot
+          </label>
+          <label
+            htmlFor="warm"
+            className="modal__label modal__label_type_radio"
+          >
+            <input
+              className="modal__radio-input"
+              type="radio"
+              id="warm"
+              required
+            />
+            Warm
+          </label>
+          <label
+            htmlFor="cold"
+            className="modal__label modal__label_type_radio"
+          >
+            <input
+              className="modal__radio-input"
+              type="radio"
+              id="cold"
+              required
+            />
+            Cold
+          </label>
+        </fieldset>
+      </ModalWithForm>
     </div>
   );
 }
