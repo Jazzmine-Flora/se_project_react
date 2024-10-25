9f357a0313486a287d3449adb4cdba23c7a1528f
Jazzmine-Flora <taliba.latif@gmail.com>
2024-10-25 17:53:34 -0400

    check res

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,14 @@
 const baseUrl = "http://localhost:3001";
 
+function checkResponse(response) {
+  if (response.ok) {
+    return response.json();
+  }
+  return Promise.reject(`Error: ${response.status}`);
+}
+
 function getItems() {
-  return fetch(`${baseUrl}/items`).then((response) => {
-    return response.ok
-      ? response.json()
-      : Promise.reject("Error: ${response.status}");
-  });
+  return fetch(`${baseUrl}/items`).then(checkResponse);
 }
 
 function addItem(item) {
@@ -15,22 +18,13 @@ function addItem(item) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(item),
-  }).then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    return Promise.reject(`Error: ${response.status}`);
-  });
+  }).then(checkResponse);
 }
 
 function deleteItem(id) {
   return fetch(`${baseUrl}/items/${id}`, {
     method: "DELETE",
-  }).then((response) => {
-    return response.ok
-      ? response.json()
-      : Promise.reject("Error: ${response.status}");
-  });
+  }).then(checkResponse);
 }
 
 const api = {
